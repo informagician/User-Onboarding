bdae2f7c657df220a2b375c45120a1535c688b94
Milo Rastgoo <hello@informagician.com>
2019-12-19 15:30:16 -0500

    form elements created in jsx

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import Form from './components/Form';
+import FormikForm from './components/Form';
 
 function App() {
   return (
     <div className="App">
-      <Form />
+      <FormikForm />
     </div>
   );
 }
--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -1,7 +1,33 @@
 import React from 'react';
+import { withFormik } from 'formik';
 
 const Form = () => {
-    return null;
+    return (
+        <form>
+            <label htmlFor="name">Name:
+                <input type="name" name="name" placeholder="Name" />
+            </label>
+
+            <label htmlFor="email">Email:
+                <input type="email" name="email" placeholder="Email" />
+            </label>
+
+            <label htmlFor="email">Password:
+                <input type="password" name="password" placeholder="Password" />
+            </label>
+
+            <label htmlFor="tos">
+                <input type="checkbox" name="tos" />
+                Terms of Service
+            </label>
+
+            <input type="button" value="Submit" />
+        </form>
+    );
 }
 
-export default Form;
+const FormikForm = withFormik({
+
+})(Form)
+
+export default FormikForm;
